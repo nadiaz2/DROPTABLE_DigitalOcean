05d5a79133b5e48a8a715b243113be114433d40b
Nelson Diaz <nadiaz2@wpi.edu>
2024-02-23 09:04:33 -0500

    Test removing Vue source files

--- Vue/src/plugins/flip.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener("deviceorientation", handleOrientation);
-
-function handleOrientation(event){
-  console.log(event.absolute);
-  if (event.beta > 100 || event.beta < -100){
-    window.location.href = 'pause.html';
-  }
-}
--- test_server.js
+++
@@ -1,9 +0,0 @@
-console.log("server ran")
-
-function sleep(time) {
-	return new Promise((resolve) => setTimeout(resolve, time))
-}
-
-while (true) {
-	await sleep(500)
-}
