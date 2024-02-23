6d02e806709d1ebd75ab02fcf97052aa7a13beb7
Azu109 <deshiming@outlook.com>
2024-02-23 09:01:25 +0800

    test main.js

--- src/main.js
+++ src/main.js
@@ -6,17 +6,18 @@ import router from "./router";
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import { registerSW } from 'virtual:pwa-register';
 
-
+console.log('1')
 const app = createApp(App);
 
 const updateSW = registerSW({
     onNeedRefresh() {},
     onOfflineReady() {},
   });
-  
-const vuetify = createVuetify(); // Initialize Vuetify
 
+const vuetify = createVuetify(); // Initialize Vuetify
+console.log('2')
 app.use(vuetify);
 app.use(router)
-
+console.log('3')
 app.mount('#app');
+console.log('4')
