9d9d92dc21590436f3c6f72af27308eeb0d92a33
Botao Han <73296496+124135417@users.noreply.github.com>
2024-04-06 13:40:02 -0400

    Removed transition to debug

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,10 +1,10 @@
 import { RouterLink, RouterView } from 'vue-router'
 
 <template>
-  <transition name="fade" mode="out-in">
+
     <RouterView :key="$route.fullPath" />
 
-  </transition>
+
 </template>
 
 <script>
